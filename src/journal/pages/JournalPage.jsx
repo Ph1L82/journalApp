8147d27386161d8d6f67d8b8c821f1c9d6274470
edu82 <eduardo@philipsoft.cl>
2023-02-16 19:05:27 -0300

    jopurnal layout

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,11 +1,11 @@
 import { MailOutline } from "@mui/icons-material"
 import { Typography } from "@mui/material"
+import { JournalLayout } from "../layout/JournalLayout"
 
 export const JournalPage = () => {
     return (
-        <>
-            <Typography variant="h1">JournalPage</Typography>
-            <MailOutline />
-        </>
+        <JournalLayout>
+            <Typography variant="h1">Irure ex veniam voluptate ullamco labore mollit excepteur dolore laboris.</Typography>
+        </JournalLayout>
     )
 }
