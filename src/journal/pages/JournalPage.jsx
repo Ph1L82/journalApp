7c6ebff9b86a1a45044cae109631825445db4995
edu82 <eduardo@philipsoft.cl>
2023-02-16 19:59:59 -0300

    sa

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,5 +1,3 @@
-import { MailOutline } from "@mui/icons-material"
-import { Typography } from "@mui/material"
 import { JournalLayout } from "../layout/JournalLayout"
 import { NothingSlectedView } from "../views/NothingSlectedView"
 
