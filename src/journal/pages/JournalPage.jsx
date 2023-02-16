293fd2392dfe400d1824eee7fb33c7594f822e61
edu82 <eduardo@philipsoft.cl>
2023-02-16 19:59:42 -0300

    empty view

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,11 +1,12 @@
 import { MailOutline } from "@mui/icons-material"
 import { Typography } from "@mui/material"
 import { JournalLayout } from "../layout/JournalLayout"
+import { NothingSlectedView } from "../views/NothingSlectedView"
 
 export const JournalPage = () => {
     return (
         <JournalLayout>
-            <Typography variant="h1">Irure ex veniam voluptate ullamco labore mollit excepteur dolore laboris.</Typography>
+            <NothingSlectedView />
         </JournalLayout>
     )
 }
