9e0582ced1efd92e8d6d764c6942acc015e46f4f
edu82 <eduardo@philipsoft.cl>
2023-02-16 19:46:04 -0300

    toolbar

--- src/journal/layout/JournalLayout.jsx
+++ src/journal/layout/JournalLayout.jsx
@@ -1,4 +1,4 @@
-import { Box } from "@mui/material"
+import { Box, Toolbar } from "@mui/material"
 import { NavBar, SideBar } from "../components";
 
 const drawerWidth = 340;
@@ -12,6 +12,7 @@ export const JournalLayout = ({ children }) => {
                 component="main"
                 sx={{ flexGrow: 1, p: 3 }}
             >
+                <Toolbar />
                 {children}
             </Box>
         </Box>
